43666f7830c24c94e740b77d50c57e39b110b534
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2021-09-30 15:58:02 +0200

    fix #19

--- pipe-modules/utils.js
+++ pipe-modules/utils.js
@@ -16,6 +16,20 @@ function processAssertions(data) {
   return data;
 }
 
+function processTesters(data) {
+  if (data.data.length > 0 && data.data[0].processor) {
+    const result = {};
+
+    data.data.forEach(el => {
+      result[el.processor] = el.assertedBy.fullname;
+    });
+    console.log(result);
+    return {data: result};
+  } else {
+    return data;
+  }
+}
+
 function formatDate(data) {
   if (data.data.length > 0 && data.data[0].version) {
     data.data.forEach(processor => {
@@ -35,17 +49,32 @@ function setRDBMS(data) {
     data.data.forEach(processor => {
       const matches = processor.name.match(/-([^-]+)$/);
       processor.rdbms = matches[1].toLowerCase();
+      processor.rdbmsCap = fixCapitalizationRDBMS(processor.rdbms);
       processor.name = processor.name.replace(matches[0], '');
     });
 
-    console.log(data);
+    //console.log(data);
   }
 
   return data;
 }
 
+const rdbms = ['MySQL', 'PostgreSQL'];
+const rdbmsLowerCase = rdbms.map(a => a.toLowerCase());
+
+function fixCapitalizationRDBMS(str) {
+  const index = rdbmsLowerCase.indexOf(str.toLowerCase());
+
+  if (index >= 0) {
+    return rdbms[index];
+  } else {
+    return str;
+  }
+}
+
 module.exports = {
   processAssertions,
   formatDate,
-  setRDBMS
+  setRDBMS,
+  processTesters
 }
